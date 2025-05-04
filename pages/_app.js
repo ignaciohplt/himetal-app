702e3ac43bd19191e90abb3a71c716049d1ad3c1
ignacio hipolito <ignaciohipolitoih@gmail.com.com>
2025-05-03 22:02:30 -0300

    Reactivar CSS global para Tailwind en Next.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-  // ajusta la ruta si tu CSS global está en otro sitio
+import '../src/index.css';  // ajusta la ruta si tu CSS global está en otro sitio
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
