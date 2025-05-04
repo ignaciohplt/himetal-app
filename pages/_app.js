76881c4561533ae8b8e8d178edc33dbeaa4ac8e1
ignacio hipolito <ignaciohipolitoih@gmail.com.com>
2025-05-03 21:42:13 -0300

    Quitar import de CSS para que compile en Next.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import '../src/index.css';  // ajusta la ruta si tu CSS global está en otro sitio
+  // ajusta la ruta si tu CSS global está en otro sitio
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
