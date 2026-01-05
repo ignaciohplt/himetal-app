3b820eab242a97f9d3cf12d83ad9ed7560a71783
ignaciohplt <ignaciohipolitoih@gmail.com>
2026-01-05 00:42:22 -0300

    Modify price data for Laminadofrio and Laminadocalienteasasasdadas
    Updated prices for Laminadofrio and Laminadocaliente.

--- src/data/sidersaData.js
+++ src/data/sidersaData.js
@@ -1,34 +1,35 @@
-// src/data/sidersaData.js
+// data/sidersaData.js
+// Datos de precios Sidersa para laminado frío y caliente
+
 export const Laminadofrio = [
-  { espesor: 0.35, precios: { "1000": 1436, "1220": null, "1500": null } },
-  { espesor: 0.40, precios: { "1000": 1411, "1220": 1374, "1500": null } },
-  { espesor: 0.45, precios: { "1000": null, "1220": 1350, "1500": null } },
-  { espesor: 0.50, precios: { "1000": null, "1220": 1337, "1500": null } },
-  { espesor: 0.55, precios: { "1000": null, "1220": 1325, "1500": null } },
-  { espesor: 0.65, precios: { "1000": 1301, "1220": 1288, "1500": null } },
-  { espesor: 0.70, precios: { "1000": 1325, "1220": 1288, "1500": 1276 } },
-  { espesor: 0.80, precios: { "1000": null, "1220": 1276, "1500": 1264 } },
-  { espesor: 0.90, precios: { "1000": 1288, "1220": 1252, "1500": 1239 } },
-  { espesor: 1.10, precios: { "1000": null, "1220": 1252, "1500": 1239 } },
-  { espesor: 1.25, precios: { "1000": 1276, "1220": 1239, "1500": 1227 } },
-  { espesor: 1.40, precios: { "1000": null, "1220": 1239, "1500": 1227 } },
-  { espesor: 1.60, precios: { "1000": 1276, "1220": 1239, "1500": 1227 } },
-  { espesor: 1.80, precios: { "1000": null, "1220": null, "1500": 1239 } },
-  { espesor: 2.00, precios: { "1000": 1288, "1220": 1252, "1500": 1239 } },
-  { espesor: 2.50, precios: { "1000": null, "1220": 1252, "1500": 1239 } },
-  { espesor: 3.00, precios: { "1000": null, "1220": null, "1500": 1239 } }
+  { espesor: 0.35, precios: { "1000": 1397, "1220": null, "1500": null } },
+  { espesor: 0.40, precios: { "1000": null, "1220": 1337, "1500": null } },
+  { espesor: 0.45, precios: { "1000": null, "1220": 1313, "1500": null } },
+  { espesor: 0.50, precios: { "1000": null, "1220": 1302, "1500": null } },
+  { espesor: 0.55, precios: { "1000": null, "1220": 1290, "1500": null } },
+  { espesor: 0.65, precios: { "1000": 1302, "1220": 1266, "1500": 1254 } },
+  { espesor: 0.70, precios: { "1000": 1290, "1220": 1254, "1500": 1242 } },
+  { espesor: 0.80, precios: { "1000": null, "1220": 1242, "1500": 1230 } },
+  { espesor: 0.90, precios: { "1000": 1254, "1220": 1218, "1500": 1206 } },
+  { espesor: 1.10, precios: { "1000": null, "1220": 1218, "1500": 1206 } },
+  { espesor: 1.25, precios: { "1000": 1242, "1220": 1206, "1500": 1194 } }, // (*)
+  { espesor: 1.40, precios: { "1000": null, "1220": 1206, "1500": 1194 } },
+  { espesor: 1.60, precios: { "1000": 1242, "1220": 1206, "1500": 1194 } },
+  { espesor: 1.80, precios: { "1000": null, "1220": null, "1500": 1206 } },
+  { espesor: 2.00, precios: { "1000": 1254, "1220": 1218, "1500": 1206 } },
+  { espesor: 2.50, precios: { "1000": null, "1220": 1218, "1500": 1206 } },
+  { espesor: 3.00, precios: { "1000": null, "1220": null, "1500": 1206 } }
 ];
 
 export const Laminadocaliente = [
-  { espesor: 1.80, precios: { "896": null, "1025": 1101, "1070": null, "1100": null, "1180": null, "1200": null, "1245": null, "1350": null, "1370": null, "1500": null } },
-  { espesor: 2.00, precios: { "896": null, "1025": 1080, "1070": null, "1100": null, "1180": 1050, "1200": null, "1245": null, "1350": null, "1370": null, "1500": null } },
-  { espesor: 2.50, precios: { "896": null, "1025": 1060, "1070": null, "1100": null, "1180": 1030, "1200": null, "1245": null, "1350": null, "1370": null, "1500": 1020 } },
-  { espesor: 2.85, precios: { "896": null, "1025": null, "1070": null, "1100": null, "1180": 1030, "1200": null, "1245": null, "1350": null, "1370": null, "1500": null } },
-  { espesor: 3.20, precios: { "896": null, "1025": 1050, "1070": null, "1100": null, "1180": 1020, "1200": null, "1245": null, "1350": null, "1370": null, "1500": 1010 } },
-  { espesor: 4.00, precios: { "896": null, "1025": null, "1070": null, "1100": null, "1180": 1010, "1200": null, "1245": null, "1350": null, "1370": null, "1500": 1000 } },
-  { espesor: 4.75, precios: { "896": null, "1025": 1040, "1070": null, "1100": null, "1180": 1010, "1200": null, "1245": null, "1350": null, "1370": null, "1500": 1000 } },
-  { espesor: 6.35, precios: { "896": null, "1025": 1050, "1070": null, "1100": null, "1180": 1020, "1200": null, "1245": null, "1350": null, "1370": null, "1500": 1010 } },
-  { espesor: 8.00, precios: { "896": null, "1025": null, "1070": null, "1100": null, "1180": null, "1200": null, "1245": null, "1350": null, "1370": null, "1500": 1010 } },
-  { espesor: 9.50, precios: { "896": null, "1025": null, "1070": null, "1100": null, "1180": null, "1200": null, "1245": null, "1350": null, "1370": null, "1500": 1020 } },
-  { espesor: 12.50, precios: { "896": null, "1025": null, "1070": null, "1100": null, "1180": null, "1200": null, "1245": null, "1350": null, "1370": null, "1500": 1020 } }
+  { espesor: 2.00, precios: { "896": null, "1025": null, "1070": 1053, "1180": null, "1200": null, "1245": null, "1350": 1023, "1370": null, "1500": null } },
+  { espesor: 2.50, precios: { "896": null, "1025": null, "1070": 1033, "1180": null, "1200": null, "1245": null, "1350": 1004, "1370": null, "1500": 994 } },
+  { espesor: 2.85, precios: { "896": null, "1025": null, "1070": null, "1180": null, "1200": null, "1245": null, "1350": 1004, "1370": null, "1500": null } },
+  { espesor: 3.20, precios: { "896": null, "1025": null, "1070": 1023, "1180": null, "1200": null, "1245": null, "1350": 994, "1370": null, "1500": 984 } },
+  { espesor: 4.00, precios: { "896": null, "1025": null, "1070": null, "1180": null, "1200": null, "1245": null, "1350": 984, "1370": null, "1500": 974 } },
+  { espesor: 4.75, precios: { "896": null, "1025": null, "1070": 1014, "1180": null, "1200": null, "1245": null, "1350": 984, "1370": null, "1500": 974 } },
+  { espesor: 6.35, precios: { "896": null, "1025": null, "1070": 1023, "1180": null, "1200": null, "1245": null, "1350": 994, "1370": null, "1500": 984 } }, // (*)
+  { espesor: 8.00, precios: { "896": null, "1025": null, "1070": null, "1180": null, "1200": null, "1245": null, "1350": null, "1370": null, "1500": 984 } },
+  { espesor: 9.50, precios: { "896": null, "1025": null, "1070": null, "1180": null, "1200": null, "1245": null, "1350": null, "1370": null, "1500": 994 } },
+  { espesor: 12.50, precios: { "896": null, "1025": null, "1070": null, "1180": null, "1200": null, "1245": null, "1350": null, "1370": null, "1500": 994 } }
 ];
